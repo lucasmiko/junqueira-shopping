d0ffbe9aa5922f5d746e07e6ab5d0c6924130034
Lucas Mikó <lucasmiko@live.com>
2020-10-30 11:41:21 -0300

    Fim da home

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,11 +2,13 @@ const btnOpen = document.querySelector('.btn-open');
 const btnClose = document.querySelector('.btn-close');
 const closeMenu = document.querySelectorAll('[data-close="menu"]');
 const menu = document.querySelector('.menu');
+const stop = document.querySelector('body');
 
 btnOpen.addEventListener('click', e => {
 
   menu.classList.add('open')
   menu.classList.remove('menu')
+  stop.classList.add('stop-scroll')
 
 
 })
@@ -15,6 +17,7 @@ btnClose.addEventListener('click', e => {
 
   menu.classList.remove('open')
   menu.classList.add('menu')
+  stop.classList.remove('stop-scroll')
 
 })
 
@@ -23,3 +26,4 @@ closeMenu.forEach(el => {
       menu.classList.remove('open');
   })
 })
+
